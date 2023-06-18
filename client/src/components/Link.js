4348c1319453beddb8b892ae90a5bc5479af894a
Ofer Lis <oferlis1@gmail.com>
2023-06-18 16:58:25 +0300

    little addition

--- client/src/components/Link.js
+++ client/src/components/Link.js
@@ -24,7 +24,7 @@ const Link = (props) => {
                 <input type='checkbox' onChange={() => handleCheckedBox(props.id, props.isRead)} checked={isChecked}/>
             </div>
             <div className='w-4/6' onClick={() => openLinkHandler(props.link)}>{props.name}</div>
-            <div className='flex flex-row h-6'>
+            <div className='w-1/6 flex flex-row h-6 place-content-between'>
                 <img src='/images/checkbox-icon.svg' alt='Mark as read' onClick={() => handleCheckedBox(props.id, props.isRead)}/>
                 <img src='/images/delete-icon.svg' alt='Delete' onClick={() => deleteLinkHandler(props.id)}/>
                 <img src='/images/edit-icon.svg' alt='edit'/>
